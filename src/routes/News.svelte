dd3e7dd59f14d747bae392b4a29485e50933f1b3
kamalika0363 <kamalikaghora14@gmail.com>
2024-02-21 00:53:07 +0530

    links

--- src/routes/News.svelte
+++ src/routes/News.svelte
@@ -6,14 +6,17 @@
 		<h1 class="text-3xl sm:text-4xl text-center font-medium leading-9">What's New</h1>
 		<div class="flex flex-col md:flex-row sm:w-full gap-8 sm:mx-auto">
 			<div class="bigCard bg-[url('./lib/images/mobile_projects.png')] flex flex-col justify-end sm:w-full h-96 border border-white/20 rounded-xl bg-no-repeat sm:bg-cover bg-center bg-clip-padding">
+				<a href="/projects">
 				<div class="bigCard">
 					<div class="cardContent px-5 py-5 rounded-b-xl bg-black/20 backdrop-blur-sm sm:backdrop-blur-md">
-						<h2 class="text-2xl font-medium leading-[38px] text-left"><a href="/projects">Active Projects</a></h2>
+						<h2 class="text-2xl font-medium leading-[38px] text-left"><a href="/projects">Active Projects</h2>
 						<h3 class="text-md font-normal leading-6 text-left text-gray-300">Mobile Projects killing it in the ecosystem</h3>
 					</div>
 				</div>
+				</a>
 			</div>
 			<div class="flex flex-col w-full h-full gap-8 mx-auto">
+				<a href="https://solanamobile.com/hardware">
 				<div class="smallCard bg-[url('./lib/images/toly_img.png')] flex flex-col justify-end w-full h-52 sm:h-44 border border-white/20 rounded-xl bg-cover bg-center">
 					<div class="cardContent px-5 py-3 rounded-b-xl bg-black/20 backdrop-blur-sm sm:backdrop-blur-md">
 						<a href="https://solanamobile.com/hardware">
@@ -22,13 +25,14 @@
 						<h3 class="text-gray-300">Checkout first crypto phone ever</h3>
 					</div>
 				</div>
+				</a>
 				<div class="smallCard bg-[url('./lib/images/hackathon.png')] flex flex-col justify-end w-full h-52 sm:h-44 border border-white/20 rounded-xl bg-cover bg-center">
+					<a href="https://build.superteam.fun/past-hackathon-winners/Mobile">
 					<div class="cardContent px-5 py-3 rounded-b-xl bg-black/20 backdrop-blur-sm sm:backdrop-blur-md">
-						<a href="https://build.superteam.fun/past-hackathon-winners/Mobile">
 							<h2 class="text-2xl">Hackathon Winners</h2>
-						</a>
 						<h3 class="text-gray-300">Previous winner mobile projects</h3>
 					</div>
+					</a>
 				</div>
 			</div>
 		</div>
