7a6656333aaee24aa27d12139682718158aa690e
kamalika0363 <kamalikaghora14@gmail.com>
2024-02-23 23:02:02 +0530

    main page

--- src/routes/News.svelte
+++ src/routes/News.svelte
@@ -1,39 +1,29 @@
 <script>
 </script>
 
-<section class="sm:w-full mb-24 sm:pt-24 pb-24 gap-16 sm:mx-auto xl:px-24">
+<section class="sm:w-full min-h-screen sm:pt-24 pb-24 gap-16 sm:mx-auto lg:px-20">
 	<div class="flex flex-col gap-8 sm:mx-auto">
 		<h1 class="text-3xl sm:text-4xl text-center font-medium leading-9">What's New</h1>
 		<div class="flex flex-col md:flex-row sm:w-full gap-8 sm:mx-auto">
-			<a
-				class="bigCard bg-[url('./lib/images/mobile_projects.png')] flex flex-col justify-end sm:w-full h-96 border border-white/20 rounded-xl bg-no-repeat sm:bg-cover bg-center bg-clip-padding"
-				href="/projects">
+			<a href="/projects" class="bigCard bg-[url('./lib/images/mobile_projects.png')] flex flex-col justify-end sm:w-full h-96 border border-white/20 rounded-xl bg-no-repeat sm:bg-cover bg-center bg-clip-padding">
 				<div class="bigCard">
-					<div class="cardContent px-5 py-5 rounded-b-xl bg-black/20 backdrop-blur-sm sm:backdrop-blur-md">
-						<h2 class="text-2xl font-medium leading-[38px] text-left">Active Projects</h2>
-						<h3 class="text-md font-normal leading-6 text-left text-gray-300">Mobile Projects killing it in the
-							ecosystem</h3>
+					<div class="cardContent p-3 rounded-b-xl bg-black/20 backdrop-blur-sm sm:backdrop-blur-md">
+						<h2 class="text-xl font-semibold leading-[38px] text-left">Active Projects</h2>
+						<h3 class="text-base font-normal leading-6 text-left text-gray-300">Mobile Projects killing it in the ecosystem</h3>
 					</div>
 				</div>
 			</a>
 			<div class="flex flex-col w-full h-full gap-8 mx-auto">
-				<a href="https://solanamobile.com/hardware">
-					<div
-						class="smallCard bg-[url('./lib/images/toly_img.png')] flex flex-col justify-end w-full h-52 sm:h-44 border border-white/20 rounded-xl bg-cover bg-center">
-						<div class="cardContent px-5 py-3 rounded-b-xl bg-black/20 backdrop-blur-sm sm:backdrop-blur-md">
-							<a href="https://solanamobile.com/hardware">
-								<h2 class="text-2xl">Saga</h2>
-							</a>
-							<h3 class="text-gray-300">Checkout first crypto phone ever</h3>
-						</div>
+				<a href="https://solanamobile.com/hardware" class="smallCard bg-[url('./lib/images/toly_img.png')] flex flex-col justify-end w-full h-52 sm:h-44 border border-white/20 rounded-xl bg-cover bg-center">
+					<div class="cardContent p-3 rounded-b-xl bg-black/20 backdrop-blur-sm sm:backdrop-blur-md">
+						<h2 class="text-xl font-semibold">Saga</h2>
+						<h3 class="text-base font-normal leading-6 text-left text-gray-300">Checkout first crypto phone ever</h3>
 					</div>
 				</a>
-				<a
-					class="smallCard bg-[url('./lib/images/hackathon.png')] flex flex-col justify-end w-full h-52 sm:h-44 border border-white/20 rounded-xl bg-cover bg-center"
-					href="https://build.superteam.fun/past-hackathon-winners/Mobile">
-					<div class="cardContent px-5 py-3 rounded-b-xl bg-black/20 backdrop-blur-sm sm:backdrop-blur-md">
-						<h2 class="text-2xl">Hackathon Winners</h2>
-						<h3 class="text-gray-300">Previous winner mobile projects</h3>
+				<a href="https://build.superteam.fun/past-hackathon-winners/Mobile" class="smallCard bg-[url('./lib/images/hackathon.png')] flex flex-col justify-end w-full h-52 sm:h-44 border border-white/20 rounded-xl bg-cover bg-center">
+					<div class="cardContent p-3 rounded-b-xl bg-black/20 backdrop-blur-sm sm:backdrop-blur-md">
+						<h2 class="text-xl font-semibold">Hackathon Winners</h2>
+						<h3 class="text-base font-normal leading-6 text-left text-gray-300">Previous winner mobile projects</h3>
 					</div>
 				</a>
 			</div>
